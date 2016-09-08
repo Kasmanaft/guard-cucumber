2aa61fc64352919448e5eab358e50a531685af97
Leonid Sobolievskyi <kasmanaft@gmail.com>
2016-09-08 19:08:19 +0300

    added priority

--- lib/guard/cucumber/notification_formatter.rb
+++ lib/guard/cucumber/notification_formatter.rb
@@ -118,7 +118,7 @@ module Guard
         icon = statuses.reverse.detect { |status| icon_for(status) }
 
         msg = messages.reverse.join(", ")
-        Guard::Compat::UI.notify msg, title: "Cucumber Results", image: icon
+        Guard::Compat::UI.notify msg, title: "Cucumber Results", image: icon, priority: 2
       end
 
       # Writes the `rerun.txt` file containing all failed features.
